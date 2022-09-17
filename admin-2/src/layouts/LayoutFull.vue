c75ed6c4b0a2602b33c0ae3f25ad0bf477efa941
Dasha <dasha@100up.ru>
2022-09-17 19:02:33 +0300

    start test

---
+++ admin-2/src/layouts/LayoutFull.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <v-container>
+            <router-view></router-view>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: "FullPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin-2/src/layouts/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-row
+                    align="center"
+                    justify="center"
+            >
+                <v-btn
+                        color="pink"
+                        dark
+                        @click.stop="drawer = !drawer"
+                >
+                    Toggle
+                </v-btn>
+            </v-row>
+        </v-app-bar>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: "the-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
